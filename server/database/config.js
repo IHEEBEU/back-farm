ed07899dc6068d4d4d50ff045626a3955584c824
IHEEB <132073063+IHEEBEU@users.noreply.github.com>
2023-10-02 20:18:02 +0100

    Update config.js

--- server/database/config.js
+++ server/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "farm"
+    host : "bqdna82yrxkr0btf7umo-mysql.services.clever-cloud.com",
+    user : "ud8afpzjvxqs5t3a",
+    password : "jZ6Ahm0kf5upLKd5XFg2",
+    database : "bqdna82yrxkr0btf7umo"
 }
